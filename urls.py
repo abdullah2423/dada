9d28b0f36ee3540cc7cf415def16e3d09def9717
abdullah2423 <162664236+abdullah2423@users.noreply.github.com>
2024-04-25 21:23:10 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+import include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # Admin interface
     path('subscribe/', views.subscribe_view, name='subscribe'),  # Subscription view
     path('payment/', views.payment_form, name='payment_form'),  # Payment form view
     path('payment/status/', views.check_payment_status, name='check_payment_status'),  # Payment status check endpoint
@@ -10,4 +13,5 @@ urlpatterns = [
     path('payment/refund/partial/<str:checkout_id>/', views.partial_refund, name='partial_refund'),  # Partial refund endpoint
     path('payment/card/check/<str:bin>/', views.check_card, name='check_card'),  # Card check endpoint
     path('cancel_subscription/<int:subscription_id>/', views.cancel_subscription, name='cancel_subscription'),  # Subscription cancellation endpoint
+    path('payments/', include('payments.urls')),  # Include URLs from 'payments' app
 ]
