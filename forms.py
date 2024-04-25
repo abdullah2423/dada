3df58796f1017c81c85086a1113c079392b63ab9
abdullah2423 <162664236+abdullah2423@users.noreply.github.com>
2024-04-25 21:13:44 +0200

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,24 +1,31 @@
 from django import forms
-from .models import Order, Currency ,Language ,CardType ,PaymentMethod
-from django.forms import CheckboxSelectMultiple
+from .models import Order, Currency, Language, CardType, PaymentMethod, Subscription
 
 class PaymentForm(forms.ModelForm):
-    class Meta:
-        currency = forms.ModelChoiceField(queryset=Currency.objects.all())
-        Language = forms.ModelChoiceField(queryset=Language.objects.all())
-        payment_methods = forms.MultipleChoiceField(
+    currency = forms.ChoiceField(choices=[(currency.pk, currency.name) for currency in Currency.objects.all()])
+    language = forms.ChoiceField(choices=[(language.pk, language.name) for language in Language.objects.all()])
+    payment_methods = forms.MultipleChoiceField(
         choices=PaymentMethod.PAYMENT_METHOD_CHOICES,
-        widget=CheckboxSelectMultiple()
+        widget=forms.CheckboxSelectMultiple()
     )
-
-        card_types = forms.MultipleChoiceField(
+    card_types = forms.MultipleChoiceField(
         choices=CardType.CARD_TYPE_CHOICES,
-        widget=CheckboxSelectMultiple(),
+        widget=forms.CheckboxSelectMultiple(),
         required=False  # Assuming card types might be optional depending on payment method
     )
+
+    class Meta:
         model = Order
         fields = [
             'amount', 'customer_name', 'customer_email', 'customer_phone',
             'payment_methods', 'card_types', 'currency', 'reference', 'language', 
-            'response_url', 'cancel_url'  # Include the new fields 
+            'response_url', 'cancel_url'
         ]
+
+class SubscriptionForm(forms.ModelForm):
+    plan = forms.ChoiceField(choices=Subscription.PLAN_CHOICES) 
+    # ... Potentially other fields for customer details
+
+    class Meta:
+        model = Subscription
+        fields = ['plan']  # You might add more fields later
